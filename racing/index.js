a9cc1a810848b5153e5a7c69b0fdac79dfe94963
unknown <dudtjr913@naver.com>
2020-11-23 23:23:19 +0900

    New : 2. 입력받은 자동차 text로 전환 완료 <description> 예외사항도 처리하였고, 더이상 입력하지 못하도록 div를 통해 text로 출력하였다 <issue-number>

--- racing/index.js
+++ racing/index.js
@@ -2,7 +2,37 @@
 
 const carNameForm = document.body.querySelector(".car_name");
 const carNameInput = carNameForm.querySelector("input");
-const numberOfTimesForm = document.body.querySelector(".times");
+const numberOfTimesWrapper = document.querySelector(".times_wrapper");
+const numberOfTimesForm = numberOfTimesWrapper.querySelector(".times");
 const numberOfTimesInput = numberOfTimesForm.querySelector("input");
 const resultText = document.body.querySelector(".result_text");
 const resultUl = document.body.querySelector(".result_ul");
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+}
+
+const checkSubmitedCarName = (carName, carsArray) => {
+  const splitCarName = carName.split(",");
+  for (const name of splitCarName) {
+    if (name.length === 0) return alert("자동차 이름이 공백입니다.");
+    if (name.match(/([^a-zA-Z])/g)) return alert("영어가 아닙니다.");
+    if (name.length >= 5) return alert("5자리를 초과합니다.");
+  }
+  return splitCarName.forEach((name) => carsArray.push(new Car(name)));
+};
+
+const enterCarName = (e) => {
+  e.preventDefault();
+  const cars = [];
+  checkSubmitedCarName(carNameInput.value, cars);
+  if (cars.length > 0) {
+    cars.forEach(() => (carNameForm.innerText = carNameInput.value));
+  }
+  carNameInput.value = "";
+};
+
+carNameForm.addEventListener("submit", enterCarName);
