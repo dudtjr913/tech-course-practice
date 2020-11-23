2d374b5660bb761a0bf7acb5b64155df39de19f5
unknown <dudtjr913@naver.com>
2020-11-23 22:34:17 +0900

    etc : 필요한 html요소들 변수에 할당 <description> <issue-number>

---
+++ racing/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const carNameForm = document.body.querySelector(".car_name");
+const carNameInput = carNameForm.querySelector("input");
+const numberOfTimesForm = document.body.querySelector(".times");
+const numberOfTimesInput = numberOfTimesForm.querySelector("input");
+const resultText = document.body.querySelector(".result_text");
+const resultUl = document.body.querySelector(".result_ul");
