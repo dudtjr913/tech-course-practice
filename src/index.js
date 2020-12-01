38962097eca21d4c1fd46de9aac868f4f52ded94
unknown <dudtjr913@naver.com>
2020-12-01 17:55:19 +0900

    feat : 정답 만드는 메소드 구현 완료 <description> <issue-number>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
+export default class BaseballGame {
+  createAnswerNumber() {
+    const array = this._createMaxNumberArray();
+    const answerArray = [];
+    while (answerArray.length !== 3) {
+      const selectedNumberIndex = this._selectRandomNumber(array.length);
+      const selectedNumber = array.splice(selectedNumberIndex, 1);
+      answerArray.push(...selectedNumber);
+    }
+
+    return answerArray;
+  }
+
+  _createMaxNumberArray() {
+    const MAX_NUMBER = 9;
+
+    return Array(MAX_NUMBER)
+      .fill()
+      .map((v, index) => index + 1);
+  }
+
+  _selectRandomNumber(range) {
+    const numberIndex = Math.floor(Math.random() * range);
+
+    return numberIndex;
+  }
+  /*play(computerInputNumbers, userInputNumbers) {
+     return "결과 값 String";
+   }*/
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
-
-new BaseballGame();
+const a = new BaseballGame();
+a.createAnswerNumber();
