a38ffce80a25ef749fa8289ee58ac5c744295f82
unknown <dudtjr913@naver.com>
2020-12-03 20:11:50 +0900

    feat : 이름 유효성 검사 함수 구현 <description> 순서대로 1. 5자리 초과일 경우 2. 특수문자나 ㅁ,ㄴ,ㅏ,ㅣ같은 말이 안되는 이름일 경우 3. 빈칸일 경우 4. 중복된 이름이 존재할 경우

--- racingGame/src/module/play.js
+++ racingGame/src/module/play.js
@@ -2,9 +2,27 @@ const $carNameSubmit = document.body.querySelector('#car-names-submit');
 
 const onSubmittedName = () => {
   const $carNameInput = document.body.querySelector('#car-names-input');
-  if (!isNameValid) {
+  if (!isNameValid($carNameInput.value)) {
     return ($carNameInput.value = '');
   }
+};
+
+const isNameValid = (value) => {
+  const splitedValue = value.split(',');
+  if (splitedValue.some((name) => name.length > 5)) {
+    return alert('5자리 이하로 입력해주세요.');
+  }
+  if (splitedValue.some((name) => name.match(/[^a-zA-Z0-9가-힣]/))) {
+    return alert('이름이 유효하지 않습니다.');
+  }
+  if (splitedValue.some((name) => name === '')) {
+    return alert('빈칸은 이름이 될 수 없습니다');
+  }
+  if (splitedValue.length !== new Set(splitedValue).size) {
+    return alert('중복된 이름이 있습니다.');
+  }
+
+  return true;
 };
 
 $carNameSubmit.addEventListener('click', onSubmittedName);
