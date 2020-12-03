0410d861ad83621998bdddfed7428811ffd04f43
unknown <dudtjr913@naver.com>
2020-12-03 20:20:32 +0900

    feat : 참가할 자동차를 입력받아 배열에 추가하는 메소드 구현 <description> 참가할 자동차의 이름을 통해 Car의 인스턴스로 생성해서 participants의 배열에 담는다.

--- racingGame/src/index.js
+++ racingGame/src/index.js
@@ -1,7 +1,13 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+import Car from './class/car.js';
 
+export default class RacingCarGame {
+  constructor() {
+    this.participants = [];
+  }
+
+  pushParticipants(cars) {
+    cars.forEach((car) => this.participants.push(new Car(car)));
+  }
+}
 
 new RacingCarGame();
