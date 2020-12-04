7aaffe7ec32a04a0b2da0c1c8ee0b1688f265772
unknown <dudtjr913@naver.com>
2020-12-04 16:31:10 +0900

    feat : 재시작 시 input.value 초기화 <description>

--- racingGame/src/module/init.js
+++ racingGame/src/module/init.js
@@ -1,6 +1,9 @@
 import {hideCountElement, hideResultElement} from './element/hideElement.js';
+import {$carNameInput, $countInput} from './utils.js';
 
 export const initGame = () => {
+  $carNameInput.value = '';
+  $countInput.value = '';
   hideCountElement();
   hideResultElement();
 };
