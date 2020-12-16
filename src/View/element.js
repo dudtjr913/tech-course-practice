8e997b2c52b6e9ac10ce9b2e7776448abad0386a
unknown <dudtjr913@naver.com>
2020-12-17 00:03:36 +0900

    Etc : 파일 삭제
    <description>

--- src/View/element.js
+++
@@ -1,9 +0,0 @@
-export const $playerNameInput = document.body.querySelector(
-  '#player-name-input',
-);
-export const $playerNameAddButton = document.body.querySelector(
-  '#player-add-btn',
-);
-export const $blackjackContainer = document.body.querySelector(
-  '#blackjack-container',
-);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Blackjack from './Model/blackjack.js';
-import {$playerNameAddButton} from './View/element.js';
-import {onSubmitPlayerName} from './Controller/play.js';
-
-export const blackjackGame = new Blackjack();
-blackjackGame.getCards();
-
-$playerNameAddButton.addEventListener('click', onSubmitPlayerName);
