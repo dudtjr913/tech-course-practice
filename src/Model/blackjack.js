0bc7fb057a9e9025b3ab4464e702a0fb6ec68f82
unknown <dudtjr913@naver.com>
2020-12-16 20:52:28 +0900

    Feat : Blackjack class 생성
    <description>
    blackjack 클래스를 생성하고, 필요한 함수들과 프로퍼티를 미리 할당해놨다.

---
+++ src/Model/blackjack.js
@@ -0,0 +1,18 @@
+export default class Blackjack {
+  constructor() {
+    this.cards = [];
+    this.players = [];
+  }
+
+  getCards() {}
+
+  betPlayer(player, amount) {}
+
+  handOutCards() {}
+
+  takeMoreCard(player) {}
+
+  findResult() {}
+
+  calculateProfit() {}
+}
