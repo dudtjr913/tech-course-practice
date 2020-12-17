3d33816b4d42ee18bdd337b1024ec5fc1d1e0b3c
unknown <dudtjr913@naver.com>
2020-12-17 10:50:40 +0900

    Feat : 참가자에게 카드를 더 받을지 응답 받는 기능 구현
    <description>
    나누어준 카드를 화면에 출력하면 이제 게임이 시작된 것이고, 베팅 입력이
    끝난 것이므로 벹팅 입력이 끝난 후 checkTakingMoreCard함수를 사용해
    카드를 더 받을지 요청을 받는다. 이때, 더 받을 화면을 만들고, y를
    입력했을 경우에는 다시 한 번 진행하고, n을 입력했을 경우 다음 참가자로
    넘어간다.

--- blackjack/src/Controller/play.js
+++ blackjack/src/Controller/play.js
@@ -1,24 +1,28 @@
 import {$playerNameInput} from '../View/element.js';
-import {addBettingScreen, addHandOutCardScreen} from '../View/addScreen.js';
+import {
+  addBettingScreen,
+  addHandOutCardScreen,
+  addCheckingMoreCardScreen,
+} from '../View/addScreen.js';
 import {isNameInputValid, isAmountValid} from './valid.js';
 import {blackjackGame} from '../index.js';
 
-let copyPlayers = [];
+let playersName = [];
 
 export const onSubmitPlayerName = () => {
   const splitedInput = $playerNameInput.value.split(',');
   if (isNameInputValid(splitedInput)) {
     blackjackGame.getPlayers(splitedInput);
-    blackjackGame.players.forEach((player) => copyPlayers.push(player.name));
+    pushPlayerName(blackjackGame.players);
 
     return getBettingAmount();
   }
 };
 
 const getBettingAmount = () => {
-  const player = copyPlayers.shift();
-  addBettingScreen(player);
-  const $button = document.body.querySelector(`#${player}-amount > button`);
+  const playerName = playersName.shift();
+  addBettingScreen(playerName);
+  const $button = document.querySelector(`#${playerName}-amount > button`);
   $button.addEventListener('click', onSubmitAmount);
 };
 
@@ -33,11 +37,43 @@ const onSubmitAmount = (e) => {
 };
 
 const checkSubmitAmountFinish = () => {
-  if (copyPlayers[0] === undefined) {
+  if (playersName[0] === undefined) {
     blackjackGame.handOutCards();
-
-    return addHandOutCardScreen(blackjackGame.players, blackjackGame.dealer);
+    addHandOutCardScreen(blackjackGame.players, blackjackGame.dealer);
+    pushPlayerName(blackjackGame.players);
+    return checktakingMoreCard();
   }
 
   return getBettingAmount();
+};
+
+const checktakingMoreCard = () => {
+  const playerName = playersName.shift();
+  addCheckingMoreCardScreen(playerName);
+  const $button = document.querySelector(`#${playerName}-more-card > button`);
+  $button.addEventListener('click', onSubmitTakingMoreCard);
+};
+
+const onSubmitTakingMoreCard = (e) => {
+  const $input = e.target.previousElementSibling;
+  if (isAnswerValid($input.value)) {
+    if ($input.value === 'y') {
+      return keepPlayingBlackjack(e.target.dataset.takeMoreCard);
+    }
+    e.target.removeEventListener('click', onSubmitTakingMoreCard);
+    return checktakingMoreCard();
+  }
+};
+
+const keepPlayingBlackjack = (playerName) => {
+  const playerCards = blackjackGame.players.find(
+    (player) => player.name === playerName,
+  ).cards;
+
+  addPlayerCardScreen(playerCards);
+  addCheckingMoreCardScreen(e.target.dataset.takeMoreCard);
+};
+
+const pushPlayerName = (players) => {
+  players.forEach((player) => playersName.push(player.name));
 };
